2cdaf2a3ccdc27a03477e4f63bd1a30c6135458f
John Luiz <joao.zenos@gmail.com>
2019-06-12 01:50:35 -0300

    Update Connection: Docstring and small changes

--- callendar/connection.py
+++ callendar/connection.py
@@ -12,23 +12,23 @@ SCOPES = ['https://www.googleapis.com/auth/calendar.readonly',
 ]
 
 def stablish_callendar_connection():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    Stablish Connection with Google Callendar API
     """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+    # created automatically when the authorization flow completes for the first time.
+
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
+
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server()
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
